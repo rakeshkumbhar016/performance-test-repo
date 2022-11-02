77e29b64c4641ec6d66deaf487dab10cacff382a
rakeshkumbhar016 <117148768+rakeshkumbhar016@users.noreply.github.com>
2022-11-02 16:09:14 +0530

    Update perf-pipeline.groovy

--- Pipelines/perf-pipeline.groovy
+++ Pipelines/perf-pipeline.groovy
@@ -17,12 +17,13 @@ node {
 
     stage('Complete any setup steps') {
         echo "Complete set-up steps"
-        echo "${octoperf_test_value}"
+       // echo "${octoperf_test_value}"
     }
 
     stage('Execute Performance Tests') {
         dir("${WORKSPACE}/scripts") {
-            bat "c:/apache-jmeter/apache-jmeter/bin/jmeter.bat -n -t Shift-Left.jmx -l Shift-Left.jtl -Joptestvalue=${octoperf_test_value}"
+            bat "c:/apache-jmeter/apache-jmeter/bin/jmeter.bat -n -t Shift-Left.jmx -l Shift-Left.jtl}"
+          //  -Joptestvalue=${octoperf_test_value
         }
     }
 
